2932d720c8bd7e49b10339b850f9b02dda3e77c9
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-04-10 09:55:28 -0700

    Create malviya.py

---
+++ malviya.py
@@ -0,0 +1,18 @@
+import*
+userbot input java import*
+  in await ("legend-os")")/n""
+  alive_msg,(send_messege,")9809876767,* 
+             os {legendhacker}[os]
+             "buttonurl://github.com/LEGEND-LX",nonetype
+             none, import (event:).userbot 
+             
+                          hack pass/
+                              pass not(int)(st(pass)send.0982324{pass.pass}
+                                             "hacker"
+                                               pass = 
+                                            true = ok"
+                                            print("OK"
+                                                 )
+                                             {
+                                                legendLegend)
+                                               
